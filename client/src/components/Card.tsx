3a56527afb8276f101df72330bbea905062626f1
Bandi Revanth <bandirevanth143@gmail.com>
2024-10-19 09:38:10 +0530

    signup-page

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({children}:any) => {
   return (
-    <div className='w-619px border-white border-1px-solid color-#3F71FF rounded-15px'>
+    <div className='w-[619px] h-full pb-20 pt-12 border border-[#3F71FF] rounded-[15px]'>
        <div className="font-[DM Sans] text-[32px] flex justify-center font-semibold">Sign Up</div> 
        <div className="font-[DM Sans] text-[16px] text-[#292929B2] flex justify-center ">Lorem Ipsum is simply dummy text</div>
         {children}
